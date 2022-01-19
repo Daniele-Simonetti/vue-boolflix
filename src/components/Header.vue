e5c22dce73d57fb8e6b0ac038676ea244c4eecdc
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-01-19 12:24:39 +0100

    add header and main structure

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="search col">
+      <input type="text" id="search" name="search" v-model="inputText" @keyup.enter="test()">
+      <button class="btn btn-primary" @click="test()">
+        Cerca
+      </button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      inputText: '',
+    };
+  },
+  methods: {
+    test() {
+      console.log(this.inputText);
+      this.inputText = '';
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,50 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Main',
+  data() {
+    return {
+      query: 'https://api.themoviedb.org/3/search/movie?api_key=071d30c82040935e095df236b751dd91&language=it-IT&query=String',
+      films: [],
+    };
+  },
+  created() {
+    // "adult": false,
+    // "backdrop_path": "/mUyRgpndM4dIpAoLApqrvXPYvaD.jpg",
+    // "genre_ids": [
+    //    27,
+    //    53,
+    //    9648
+    //  ],
+    // "id": 560014,
+    // "original_language": "en",
+    // "original_title": "The Black String",
+    // "overview": "",
+    // "popularity": 13.638,
+    // "poster_path": "/AmRX8WLUIm2Lm0i6dQrwmBWjW9A.jpg",
+    // "release_date": "2018-10-25",
+    // "title": "The Black String",
+    // "video": false,
+    // "vote_average": 5.3,
+    // "vote_count": 22
+    // },
+    axios.get(this.query)
+      .then((result) => {
+        console.log(result.data.results);
+        this.films = result.data.results;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style>
+
+</style>
