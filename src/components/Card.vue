8a49201103b5a408055a5d983d56cb55f9eebf64
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-01-21 15:35:54 +0100

    add images

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,27 +3,31 @@
     <div
       class="card "
     >
-      <img
-        :src="getImg()"
-        :alt="datas.title || datas.name"
-      >
-      <p>
-        title: {{ (datas.title) ? datas.title : datas.name }}
-      </p>
-      <p>
-        original title: {{ (datas.original_title) ?datas.original_title : datas.original_name }}
-      </p>
-      <img
-        v-if="getFlag()"
-        :src="require(`../assets/img/${datas.original_language}.png`)"
-        :alt="datas.original_language"
-      >
-      <p v-else>
-        language: {{ datas.original_language }}
-      </p>
-      <p>
-        {{ datas.vote_average }}
-      </p>
+      <div class="poster">
+        <img
+          :src="getImg()"
+          :alt="datas.title || datas.name"
+        >
+      </div>
+      <div class="text">
+        <p>
+          title: {{ (datas.title) ? datas.title : datas.name }}
+        </p>
+        <p>
+          original title: {{ (datas.original_title) ? datas.original_title : datas.original_name }}
+        </p>
+        <img
+          v-if="getFlag()"
+          :src="require(`../assets/img/${datas.original_language}.png`)"
+          :alt="datas.original_language"
+        >
+        <p v-else>
+          language: {{ datas.original_language }}
+        </p>
+        <p>
+          {{ datas.vote_average }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -43,8 +47,8 @@ export default {
         'bn',
         'it',
         'ru',
+        'fr',
       ],
-      noImgAvailable: '../assets/img/placeholder.png',
     };
   },
   methods: {
@@ -56,14 +60,18 @@ export default {
     },
     getImg() {
       if (this.datas.backdrop_path === null) {
-        return 'require(noImgAvailable)';
+        // eslint-disable-next-line global-require
+        return require('../assets/img/placeholder.png');
       }
-      return this.datas.backdrop_path;
+      return `https://image.tmdb.org/t/p/w300/${this.datas.backdrop_path}`;
     },
   },
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
 
 </style>
